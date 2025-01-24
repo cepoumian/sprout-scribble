a4e512e5168285f25ea0f44f102c00cdd9847d02
cepoumian <cepoumian@gmail.com>
2025-01-24 10:10:51 -0600

    Fork commit

--- server/actions/get-posts.ts
+++ server/actions/get-posts.ts
@@ -5,7 +5,7 @@ import { db } from "@/server"
 export default async function getPosts() {
   const posts = await db.query.posts.findMany()
   if (!posts) {
-    return { error: "Posts not found" }
+    return { error: "No posts found" }
   }
   return { success: posts }
 }
